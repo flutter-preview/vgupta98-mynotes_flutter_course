8e25c764ac71fa076bf00c82fcd7fec9ca497291
Vishal <vguptakota1998@gmail.com>
2023-07-03 15:52:40 +0530

    add crud database for application

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectoryExcpetion implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistsException implements Exception {}
+
+class CouldNotFindUserExcetion implements Exception {}
+
+class CouldNotDeteleNoteException implements Exception {}
+
+class CouldNotFindNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
